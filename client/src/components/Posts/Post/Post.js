d49f9a7837322ef4f6fcc5ce940b68f835088a1f
KollarMichall <kollar.michal21@gmail.com>
2021-11-16 15:45:59 +0100

    new feature

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -2,9 +2,9 @@ import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@
 import React from 'react'
 import useStyle from './styles.js'
 import moment from 'moment'
-import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
-import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt'
-import DeleteIcon from '@material-ui/icons/Delete'
+import MoreHorizIconOutlined from '@material-ui/icons/MoreHorizOutlined'
+import ThumbUpAltOutlined from '@material-ui/icons/ThumbUpAltOutlined'
+import DeleteOutlined from '@material-ui/icons/DeleteOutlined'
 import { useDispatch } from 'react-redux'
 import { deletePost, likePost } from '../../../actions/posts.js'
 
@@ -12,33 +12,19 @@ const Post = ({post, setCurrentId}) => {
     const classes = useStyle()
     const dispatch = useDispatch()
     return (
+   
         <Card className={classes.card}>
-            <CardMedia className={classes.media} image={post.selectedFile} title={post.title}></CardMedia>
-            <h1>{post.title}</h1>
-            <p>{post.message}</p>
-            <b>{post.tags.map(tag => `#${tag}`)}</b>
-            <Button onClick={() => {setCurrentId(post._id)}}>click</Button>
-            <Button size="small" color='primary' onClick={() => dispatch(deletePost(post._id))}>
-                    
-                   Delete
-                </Button>
-                <Button size="small" color='primary' onClick={() => dispatch(likePost(post._id))}>
-                    &nbsp; Like &nbsp;
-                    {post.likeCount}
-                </Button>
-        </Card>
-        // <Card>
-        /* <CardMedia className={classes.media} image={post.selectedFile} title={post.title}></CardMedia> */
-            /* <div className={classes.overlay}>
+         <CardMedia className={classes.media} image={post.selectedFile} title={post.title}></CardMedia> 
+             <div className={classes.overlay}>
                 <Typography variant="h6">{post.creator}</Typography>
-                <Typography variant="body2">{moment(post.createdAt)}</Typography>
-            </div> */
-            /* <div className={classes.overlay2}>
+                <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+            </div> 
+             <div className={classes.overlay2}>
                 <Button style={{color: 'white'}} size="small" onClick={() => {setCurrentId(post._id)}}>
-                    <MoreHorizIcon fontSize="medium"/>
+                    <MoreHorizIconOutlined fontSize="medium"/>
                 </Button>
-            </div> */
-            /* <div className={classes.details}>
+            </div> 
+             <div className={classes.details}>
                 <Typography variant="body2" color="textSecondary">{post.tags.map(tag => `#${tag}`)}</Typography>
             </div>
             <Typography className={classes.title} variant="h5" gutterBottom>{post.title}</Typography>
@@ -46,17 +32,17 @@ const Post = ({post, setCurrentId}) => {
                 <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size="small" color='primary' onClick={() => {}}>
-                    <ThumbUpAltIcon fontSize="small"/>
+                <Button size="small" color='primary' onClick={() => dispatch(likePost(post._id))}>
+                    <ThumbUpAltOutlined fontSize="small"/>
                     &nbsp; Like &nbsp;
                     {post.likeCount}
                 </Button>
-                <Button size="small" color='primary' onClick={() => {}}>
-                    <DeleteIcon fontSize="small"/>
+                <Button size="small" color='primary' onClick={() => dispatch(deletePost(post._id))}>
+                    <DeleteOutlined fontSize="small"/>
                    Delete
                 </Button>
-            </CardActions> */
-    // </Card>
+            </CardActions> 
+     </Card>
         )
 }
 
--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ import  cors from "cors";
 import  bodyParser from "body-parser";
 import  mongoose from "mongoose";
 import postRouter from "./routes/posts.js";
+import dotenv from "dotenv";
 
 const app = express()
+dotenv.config()
 
 app.use(bodyParser.json({limit: "32mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "32mb", extended: true}))
@@ -13,8 +15,7 @@ app.use(cors())
 app.use('/posts', postRouter)
 
 
-const CONNECT_URL = 'mongodb+srv://kollim:alzbetka21@cluster0.e1mws.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(CONNECT_URL).then(() => app.listen(PORT, () => console.log(`Server is listen on port: ${PORT}`)))
+mongoose.connect(process.env.CONNECT_URL).then(() => app.listen(PORT, () => console.log(`Server is listen on port: ${PORT}`)))
 .catch(err => console.log(err.message))
